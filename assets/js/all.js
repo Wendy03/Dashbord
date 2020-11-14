cee2935aa23d21aad8e70e9e15e983cc236a99c3
wendy03 <wendysu03@gmail.com>
2020-11-15 02:44:37 +0800

    Update 2020-11-14T18:44:34.946Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,6 +2,6 @@
 
 var url = window.location.pathname;
 $('.nav-item').find('a').each(function () {
-  $(this).toggleClass('nav-link--active', $(this).attr('href') == url.split("/")[1]);
+  $(this).toggleClass('nav-link--active', $(this).attr('href') == url.split("/Dashbord/")[1]);
 });
 //# sourceMappingURL=all.js.map
