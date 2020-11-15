10d1676b8c634691b77ceecbdcc950145d6fd6af
wendy03 <wendysu03@gmail.com>
2020-11-15 13:39:30 +0800

    Update: 修正HTML, CSS 格式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,10 @@
 const url = window.location.pathname;
 
-$('.nav-item').find('a').each(function() {
-  $(this).toggleClass('nav-link--active', $(this).attr('href') == url.split("/")[1]);
-});
-
-
+$('.nav-item')
+  .find('a')
+  .each(function () {
+    $(this).toggleClass(
+      'nav-link--active',
+      $(this).attr('href') == url.split('/')[1]
+    );
+  });
